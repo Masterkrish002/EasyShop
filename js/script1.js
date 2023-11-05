2588325cbbf7ab13e4b2014e48629bc709a806df
Masterkrish002 <122552465+Masterkrish002@users.noreply.github.com>
2023-11-05 10:50:36 +0530

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -28,8 +28,11 @@ function create() {
   document.querySelector("h3").innerHTML = "SIGNUP NOW";
   document.querySelector("#name").style.display = "inline";
   document.querySelector("#name1").style.display = "inline";
+  document.querySelector("#name2").style.display = "inline";
   document.querySelector(".para").style.display = "none";
   document.querySelector(".para1").style.display = "none";
+  document.querySelector(".forget").style.display = "none";
+  document.querySelector(".forget-btn").style.display = "none";
   document.querySelector(".btn").style.display = "none";
   document.querySelector(".btn1").style.display = "inline";
 }
@@ -52,19 +55,27 @@ function sign() {
   var n1 = document.getElementById("name").value;
 
   var n2 = document.getElementById("name1").value;
+  var n3 = document.getElementById("name2").value;
 
-  if (n1 == "" || n2 == "" || email == "" || password == "") {
+  if (n1 == "" || n2 == "" || email == "" || password == "" ||n3 == "" ) {
     alert("Fields can't be Empty");
   } else {
 
     var mailformat =
       /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     if (email.match(mailformat)) {
-      window.location.replace("home.html");
+      if(n3!=password){
+        alert("Both passwords must be same.");
+        
+      }else{
+        window.location.replace("home.html");
+      }
+      
       
     } else {
       alert("Invalid email address.");
     }
    
   }
+  
 }
